1bf5123e6c5fa3a2697f85cf08fa23e7d4e41b73
syuwawa <kobayashiasuka.general2@gmail.com>
2025-04-14 08:21:10 +0900

    header

---
+++ src/app/MainPage.tsx
@@ -0,0 +1,19 @@
+export default function MainPage() {
+  return (
+    <div>
+      <p>Your Location Tokyo</p>
+      <p>Pick a category</p>
+      <div className="grid grid-cols-4 gap-4">
+  <div>01</div>
+  <div>02</div>
+  <div>03</div>
+  <div>04</div>
+  <div>05</div>
+  <div>06</div>
+  <div>07</div>
+  <div>08</div>
+  <div>09</div>
+</div>
+    </div>
+  );
+}	
---
+++ src/app/page.tsx
@@ -0,0 +1,48 @@
+"use client"
+import Image from 'next/image';
+import { useState } from "react";
+import { ChevronDown } from "lucide-react";
+import MainPage from "./MainPage";
+
+export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className=''>
+      <div className='flex justify-around items-center p-4'>
+      <Image
+        src="/logo.png"
+        alt="J-Classified Logo"
+        width={300}
+        height={100}
+        priority
+      />
+      <div className="relative hover:cursor-pointer">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="flex items-center gap-2 px-4 py-2 bg-white shadow-sm font-size-18 hover:bg-gray-50 transition-colors hover:cursor-pointer"
+        >
+          English
+          <ChevronDown size={16} className={`transition-transform duration-200 ${isOpen ? 'rotate-180' : ''}`} />
+        </button>
+
+        <div className={`absolute right-0 mt-2 w-48 bg-white border border-gray-200 shadow-lg transition-all duration-200 ease-in-out transform ${isOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-2 pointer-events-none'}`}>
+          <div className="py-4">
+            <button className="block w-full px-4 py-2 text-left text-md hover:bg-gray-100 cursor-pointer pl-6">
+              日本語
+            </button>
+            <button className="block w-full px-4 py-2 text-left text-md hover:bg-gray-100 cursor-pointer pl-6">
+              中文
+            </button>
+            <button className="block w-full px-4 py-2 text-left text-md hover:bg-gray-100 cursor-pointer pl-6">
+              한국어
+            </button>
+          </div>
+        </div>
+      </div>
+      </div>
+      <MainPage />
+    </div>
+    
+  );
+}
